20ec70289794f8af05d69ececd0667c6ca575edc
Henn <hennogyrchik@gmail.com>
2023-03-07 11:23:00 +0300

    1_3

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("N=");
+int n = int.Parse(Console.ReadLine()!);
+
+int count = -n;
+
+while (count != n)
+{
+    Console.WriteLine(count);
+    if (n < 0)
+    {
+        count--;
+    }
+    else
+    {
+        count++;
+    }
+
+}
