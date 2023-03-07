14cc2ec82416b03f96141cd6361d7f4ea594747e
Henn <hennogyrchik@gmail.com>
2023-03-07 12:57:57 +0300

    HW 1_1

---
+++ Lesson_1/HW/1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("a=");
+int a=int.Parse(Console.ReadLine()!);
+Console.Write("b=");
+int b=int.Parse(Console.ReadLine()!);
+if (a>b){
+    Console.WriteLine("max="+a);
+}else {
+    Console.WriteLine("max="+ b);
+}
