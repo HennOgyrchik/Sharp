2b1e4001809de9ef3440b1cb0c84aaeb79d3e7e6
Henn <hennogyrchik@gmail.com>
2023-03-10 11:50:11 +0300

    HW 2_1

---
+++ Lesson_2/HW/2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int n= new Random().Next(10,10000);
+
+int SecondDigital (int num){
+    Console.WriteLine(num);
+    return num%100/10;
+}
+
+Console.Write(SecondDigital(n));
