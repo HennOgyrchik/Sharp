77ab8e986eca6e40e9b879f2bff846dc70bc4032
Henn <hennogyrchik@gmail.com>
2023-03-10 12:11:07 +0300

    HW 2_3

---
+++ Lesson_2/HW/2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет
+
+Console.Write("n=");
+int n = int.Parse(Console.ReadLine()!);
+
+
+void MyFunc(int num)
+{
+    Console.WriteLine(num);
+    int res = num / 100 % 10;
+    if (res != 0)
+    {
+        Console.Write(res);
+    }
+    else
+    {
+        Console.Write("Третьей цифры нет");
+    }
+}
+MyFunc(n);
