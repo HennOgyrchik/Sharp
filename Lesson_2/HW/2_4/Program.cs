6b7b3a6b862e93f45d742837187db8409f942e4e
Henn <hennogyrchik@gmail.com>
2023-03-10 12:45:17 +0300

    HW 2_4

--- Lesson_2/HW/2_4/Program.cs
+++ Lesson_2/HW/2_4/Program.cs
@@ -1,3 +1,15 @@
 ﻿//Напишите программу, которая принимает на вход цифру, 
 //обозначающую день недели, и проверяет, является ли этот день выходным.
 
+Console.Write("Введите число, обозначающее день недели: ");
+int day= int.Parse(Console.ReadLine()!);
+
+void Weekend (int n){
+    if (n==6 || n==7){
+        Console.Write("Да");
+    }else {
+         Console.Write("Нет");
+    }
+}
+
+Weekend(day);
