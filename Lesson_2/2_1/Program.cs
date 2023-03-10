1ba34acc57ac0dfed3a7330ffe37a84532435b91
Henn <hennogyrchik@gmail.com>
2023-03-10 10:44:09 +0300

    2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿int MaxNum (int num){
+    Console.WriteLine(num);
+if (num/10 > num%10) {
+    return num/10;
+}
+
+return num%10;
+}
+
+int n=new Random().Next(10,100);
+Console.Write(MaxNum(n));
