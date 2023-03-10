bd10ae7a71e5c05586eeca1b9237ab786ed6e93b
Henn <hennogyrchik@gmail.com>
2023-03-10 10:56:17 +0300

    2_2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("first=");
+int first = int.Parse(Console.ReadLine()!);
+Console.Write("second=");
+int second = int.Parse(Console.ReadLine()!);
+
+
+if (first%second==0){
+    Console.WriteLine("Кратно");
+} else {
+    Console.WriteLine("Не кратно, остаток "+ first%second);
+}
+
